1a2446c0da04963e1125a64a14d6eaa373ff49d6
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-26 12:24:11 +0530

    Added :Prime number

--- src/com/bridgelabz/PrimeNumber.java
+++ src/com/bridgelabz/PrimeNumber.java
@@ -1,2 +1,23 @@
-package com.bridgelabz;public class PrimeNumber {
+package com.bridgelabz;
+
+public class PrimeNumber {
+    public static boolean isPrime(int number){
+         if (number<=1){
+             return false;
+         }
+        for (int i = 2; i <=Math.sqrt(number); i++) {
+            if (number%i==0){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i <100; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
 }
