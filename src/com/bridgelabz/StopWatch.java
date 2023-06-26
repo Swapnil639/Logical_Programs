209a6346d27dafc8e944afdbe1c836db1cd4f8d7
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-26 12:27:33 +0530

    Added :Print elapsed time

--- src/com/bridgelabz/StopWatch.java
+++ src/com/bridgelabz/StopWatch.java
@@ -1,2 +1,13 @@
-package com.bridgelabz;public class StopWatch {
+package com.bridgelabz;
+
+public class StopWatch {
+    public static void main(String[] args) {
+        long startTime=System.currentTimeMillis();
+        for (int i = 0; i <=100; i++) {
+            System.out.println(i);
+        }
+        long endTime=System.currentTimeMillis();
+        long elapseTime=endTime-startTime;
+        System.out.println("Elapse Time : "+elapseTime);
+    }
 }
