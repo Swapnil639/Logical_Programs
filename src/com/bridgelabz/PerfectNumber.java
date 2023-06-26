5660d66ac383d78dea77716bb52e8b3626fc881e
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-26 12:22:51 +0530

    Added :Perfect number

--- src/com/bridgelabz/PerfectNumber.java
+++ src/com/bridgelabz/PerfectNumber.java
@@ -1,2 +1,24 @@
-package com.bridgelabz;public class PerfectNumber {
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int number=scanner.nextInt();
+        int temp=number;
+        int sum=0;
+        for (int i = 1; i <temp; i++) {
+            if (temp%i==0){
+                System.out.println(i);
+               sum=sum+i;
+            }
+        }
+        System.out.println("Sum of Positive divisors : "+sum);
+        if (number==sum){
+            System.out.println(number+" is Perfect number");
+        }else {
+            System.out.println(number+" is not Perfect number");
+        }
+    }
 }
