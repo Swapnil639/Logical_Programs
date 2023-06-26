43a0203b961b727ad450023f9af8e2213455ba2a
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-26 12:25:25 +0530

    Added :Reverse Order

--- src/com/bridgelabz/ReverseNumber.java
+++ src/com/bridgelabz/ReverseNumber.java
@@ -1,2 +1,21 @@
-package com.bridgelabz;public class ReverseNumber {
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int number=scanner.nextInt();
+        int rem,rev=0;
+        while (number!=0){
+            rem=number%10;
+            rev=rev*10+rem;
+            number=number/10;
+        }
+        System.out.println("Reverse Number : "+rev);
+        int n=2;
+        int value=(int) Math.sqrt(n);
+        System.out.println(value);
+    }
 }
