ba7ee0ad13bcdec1378d7631594f8b605b920b85
Swapnil639 <swapnilpatil631997@gmail.com>
2023-06-26 12:26:35 +0530

    Added :Generate coupon number

--- src/com/bridgelabz/CouponNumber.java
+++ src/com/bridgelabz/CouponNumber.java
@@ -1,2 +1,18 @@
-package com.bridgelabz;public class CouponNumber {
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class CouponNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many coupons to generate ?");
+        int howManyCoupon = scanner.nextInt();
+        int min = 100;
+        int max = 999;
+        System.out.println("Coupon Number : ");
+        for (int i = 0; i < howManyCoupon; i++) {
+            int couponNumber = (int) (Math.random() * (max - min) + min);
+            System.out.println(couponNumber);
+        }
+    }
 }
